cb076d136ad28067cf7d1559bac0b4f1cc47b19f
PaulLyu <polina.lyubeznova@geogracom.com>
2025-06-17 22:20:06 +0300

    feat(componetns/ui/Button): добавила компонент кнопки

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { GameStatus } from './GameStatus';
 export { PlayerScore } from './PlayerScore';
 export { Square } from './Square';
 export { Checker } from './Checker';
+export { Button } from './Button';
